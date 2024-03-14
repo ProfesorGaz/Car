44f5b86c9aa92b7280b35e2627a703c8212e4122
ProfesorGaz <d.juan.gaz@cesba-queretaro.edu.mx>
2024-03-13 20:14:22 -0600

    3º Commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,7 +3,7 @@ public class test {
     
     public static void main(String[] args) {
         
-        System.out.println("Esta es una prueba para GitHub");
+        System.out.println("Esta es una prueba para GitHub : 8:13 pm");
     }
     
 }
